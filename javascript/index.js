3d27beb28fdbe37f1e3bb263932c3d29b01da7e1
Luca Di Vittorio <luca.div@hotmail.com>
2022-10-03 19:08:56 +0100

    javascript completed drum kit working well

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,20 @@
-window.addEventListener('keydown', (event) => {
-  console.log(event.code)
-})
+function play(event) {
+  const code = event.code;
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+  if(!audio) return; // This will stop the function if audio not found!
+
+  const key = document.querySelector(`.key[data-key="${code}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+window.addEventListener('keydown', play);
+
+const keys = document.querySelectorAll('.key');
+
+function removeTransition(event) {
+  this.classList.remove('playing');
+}
+
+keys.forEach( key => key.addEventListener('transitionend', removeTransition));
